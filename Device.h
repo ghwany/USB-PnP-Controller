d279e644d39869ecae46c58ef802f88af53802e4
elegnate <37270569+elegnate@users.noreply.github.com>
2018-08-08 20:41:13 +0900

    Update Device.h

--- Device.h
+++ Device.h
@@ -11,12 +11,14 @@
 #define INSTANCEID_MAXSIZE	50
 #define DEVICEID_MAXSIZE	31
 
-// USB ����� GUID�� �Դϴ�.
+#define CHECK_BIT(_value, _bit) ((_value & _bit) == _bit)
+
+// USB 장비의 GUID값 입니다.
 DEFINE_GUID(GUID_DEVCLASS_USB, 0x36fc9e60L, 0xc465, 0x11cf, 0x80, 0x56, 0x44, 0x45, 0x53, 0x54, 0x00, 0x00);
 
 
 namespace device {
-	// ��ġ���� ENUM
+	// 장치상태 ENUM
 	enum DEVICE_STATE {
 		DV_DEFAULT	= 0x0000,
 		DV_MOUNT	= 0x0001,
@@ -56,21 +58,21 @@ namespace device {
 
 	protected:
 
-		// ��ġ�� GUID
-		// �ڽ�Ŭ�������� �߰��� �ʱ�ȭ�մϴ�.
+		// 장치의 GUID
+		// 자식클래스에서 추가로 초기화합니다.
 		const GUID *m_cpGuid;
 
 
-		// ������ ��ġ�� ���¸� �����մϴ�.
+		// 선택한 장치의 상태를 설정합니다.
 		BOOL ChangeState(DWORD IN dwState, PCONN_DEV IN OUT pConnDev);
-		// ��ġ������ �����ϱ� ���� �ڵ鿡 ����մϴ�.
-		// ����ϱ� �� pConnDev�� �޸𸮰� �Ҵ�Ǿ� �־�� �մϴ�.
+		// 장치정보에 접근하기 위해 핸들에 등록합니다.
+		// 등록하기 전 pConnDev에 메모리가 할당되어 있어야 합니다.
 		LSTATUS Attach(PCONN_DEV OUT pConnDev);
-		// ����� ��ġ�� �и��մϴ�.
+		// 연결된 장치를 분리합니다.
 		void Detach(PCONN_DEV IN pConnDev = NULL);
-		// ��ġ���� �Լ��Դϴ�.
+		// 장치열거 함수입니다.
 		DEVICE_STATE EnumDevice(LPARAM IN OUT lParam = NULL);
-		// ��ġ���� �Լ����� ������ ���� ���⿡ �����մϴ�.
+		// 장치열거 함수에서 수행할 일을 여기에 구현합니다.
 		virtual DEVICE_STATE EnumProc(PCONN_DEV IN OUT pConnDev, LPARAM IN OUT lParam) = 0;
 	};
 }
